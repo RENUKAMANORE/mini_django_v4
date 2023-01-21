5aa02b1a626361ec97a2f54d566f012debb81d1f
RENUKAMANORE <renukamanore10@gmail.com>
2023-01-21 08:01:07 +0530

    relations

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Portal, Title
+from .models import Portal, Title,JobDescription,Applicant
 
 # Register your models here.
 admin.site.register(Portal)
 admin.site.register(Title)
+admin.site.register(JobDescription)
+admin.site.register(Applicant)
+
+
--- jobs/models.py
+++ jobs/models.py
@@ -6,15 +6,33 @@ from django.contrib.auth .models import User
 
 class Portal(models.Model):
     name = models.CharField(max_length=250,unique=True)
-    discription = models.CharField(max_length=250)
+    description = models.CharField(max_length=250)
 
     def launch(self):
         self.save()
 
     def __str__(self):
-        return self.name + "  " + self.description
+        return self.name
 
 class Title(models.Model):
     roll_no = models.CharField(max_length=250)
     last_updated = models.DateTimeField(default=timezone.now())
     portal = models.ForeignKey(Portal, on_delete=Portal)
+
+class JobDescription(models.Model):
+    rol = models.CharField(max_length=250)
+    description = models.CharField(max_length=250)
+    pub_date = models.DateTimeField(default=timezone.now())
+
+    def __str__(self):
+        return self.rol
+
+class Applicant(models.Model):
+    job_description = models.ForeignKey(
+        JobDescription , on_delete=models.CASCADE)
+    cover_letter = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.job_description
+
+
